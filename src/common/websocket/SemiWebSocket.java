e26958434c213dd58534123c09247064ee796278
wizvee <jeonghye.dev@gmail.com>
2019-09-22 20:36:00 +0900

    fixed

--- src/common/websocket/SemiWebSocket.java
+++ src/common/websocket/SemiWebSocket.java
@@ -1,14 +1,8 @@
 package common.websocket;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.websocket.EncodeException;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-
-import com.google.gson.Gson;
 
 @ServerEndpoint(value = "/ws")
 public class SemiWebSocket {
