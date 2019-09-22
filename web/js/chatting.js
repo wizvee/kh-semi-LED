47f64da3e64fa5f5546786c69572ce64f52e54a8
wj2kim <wj2kim@gmail.com>
2019-09-22 23:14:55 +0900

    update from woojung

--- web/js/chatting.js
+++ web/js/chatting.js
@@ -23,19 +23,4 @@ btnChatBack.addEventListener("click", () => {
   msgArea.style.display = "none";
 });
 
-const dropToggle = selectElements(".dropdown_toggle");
-dropToggle.map(e => {
-  e.addEventListener("click", ({ target }) => {
-    const menu = target.nextElementSibling;
-    if (menu.style.display == "none") menu.style.display = "block";
-    else menu.style.display = "none";
-  });
-});
-
-function selectElements(name) {
-  return Array.from(document.querySelectorAll(name));
-}
-
-$(".fa-sign-out").on("click", function() {
-  location.href = "/p_190826_semi/logout.do";
-});
+const chatting="";
