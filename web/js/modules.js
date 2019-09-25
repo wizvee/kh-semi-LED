941b5e2694464610db0eefe55cf91693d57df56d
wizvee <jeonghye.dev@gmail.com>
2019-09-25 15:49:24 +0900

    fix

--- web/js/modules.js
+++ web/js/modules.js
@@ -2,9 +2,7 @@ let socket = new WebSocket("ws://localhost:9090/p_190826_semi/ws");
 
 socket.onmessage = e => {
   console.log(e.data);
-  //   const flag = e.data;
-  //   if (e.data == "noti")
-  //     document.querySelector("#gnb_alert").setAttribute("class", "news");
+  
 };
 
 const selectElements = src => Array.from(document.querySelectorAll(src));
