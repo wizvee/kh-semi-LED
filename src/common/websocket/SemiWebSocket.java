f06184b1a4cebf6d5c32552ff59f1e9d0835341a
wizvee <jeonghye.dev@gmail.com>
2019-09-27 20:59:50 +0900

    WebSocket 접속 시 HttpSession 적용 -정혜

--- src/common/websocket/SemiWebSocket.java
+++ src/common/websocket/SemiWebSocket.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 import javax.servlet.http.HttpSession;
+import javax.websocket.EndpointConfig;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
@@ -11,14 +12,15 @@ import javax.websocket.server.ServerEndpoint;
 import com.semi.noti.model.service.NotiService;
 import com.semi.userinfo.model.vo.UserInfo;
 
-@ServerEndpoint(value = "/ws", encoders = { UserInfoEncoder.class }, decoders = { UserInfoDecoder.class })
+@ServerEndpoint(value = "/ws", configurator = GetHttpSession.class, encoders = { UserInfoEncoder.class }, decoders = {
+		UserInfoDecoder.class })
 public class SemiWebSocket {
 
 	private static final HashMap<String, Session> USERS = new HashMap<>();
 	private static int count = 0;
 
 	@OnMessage
-	public void onMessage(Session session, UserInfo userInfo) {
+	public void onMessage(Session session, EndpointConfig config, UserInfo userInfo) {
 		String flag = userInfo.getFlag();
 		if (flag.equals("S")) {
 			USERS.put(userInfo.getUserId(), session);
@@ -37,10 +39,9 @@ public class SemiWebSocket {
 			HashSet<String> userSet = new NotiService().getAlertTarget(userInfo.getSelectBusId());
 			for (String id : userSet) {
 				if (USERS.get(id) != null) {
-					System.out.println("T");
-					HttpSession hs = (HttpSession) USERS.get(id);
+					HttpSession hs = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
 					UserInfo ui = (UserInfo) hs.getAttribute("userInfo");
-					System.out.println(ui);
+					System.out.println(ui.getSelectBusId());
 				}
 			}
 		}
